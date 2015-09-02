232e84dd9d0b4186250877fc44154cb9ad29474f
Satoshi Hasegawa <shasegawa@yourinventit.com>
2015-09-02 11:46:40 +0900

    fix error logs.

--- src/file/file_downloader.c
+++ src/file/file_downloader.c
@@ -612,7 +612,7 @@ TFILEDownloader_CallOnCompleteCallback(TFILEDownloader *self)
       TFILEDownloader_StoreResultCode(self, FILE_ERROR_OK, "Downloading file has been complated successfuly.", sse_true);
     } else {
       LOG_ERROR("Downloding file has been failed.");
-      MOAT_VALUE_DUMP_ERROR(TAG, self->fResultCode);
+      MOAT_OBJECT_DUMP_ERROR(TAG, self->fResultCode);
     }
     err_code = moat_object_get_value(self->fResultCode, "err_code");
     err_msg  = moat_object_get_value(self->fResultCode, "err_msg");
